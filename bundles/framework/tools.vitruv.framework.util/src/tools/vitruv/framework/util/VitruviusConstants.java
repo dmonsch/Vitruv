f07a1c37ced44ea4732e5f243ed896a5c6dd2b85
Sofia Ananieva <ananieva@fzi.de>
2020-05-11 14:56:54 +0200

    add vave-plugin to framework

--- bundles/framework/tools.vitruv.framework.util/src/tools/vitruv/framework/util/VitruviusConstants.java
+++ bundles/framework/tools.vitruv.framework.util/src/tools/vitruv/framework/util/VitruviusConstants.java
@@ -7,6 +7,7 @@ public class VitruviusConstants {
 	private static final String PATHMAP_PREFIX = "pathmap:/";
 	private static final String PLATFORM_RESOURCE_PREFIX = "platform:/resource/";
 	private static final String CORRESPONDENCES_FILE_EXT = "correspondence";
+	private static final String VAVE_FILE_EXT = "vavemodel";
 	private static final String UUID_FILE_EXT = "uuid";
 	private static final String DEFAULT_NAME_OF_IDENTIFIER_FEATURE = "id";
 	private static final String Tuid_SEGMENT_SEPERATOR = "#";
@@ -42,6 +43,10 @@ public class VitruviusConstants {
 
 	public static String getCorrespondencesFileExt() {
 		return CORRESPONDENCES_FILE_EXT;
+	}
+	
+	public static String getVaVeFileExt() {
+		return VAVE_FILE_EXT;
 	}
 
 	public static String getUuidFileExt() {
